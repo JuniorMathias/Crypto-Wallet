bc83e202cc14291976b673e379688a0e31966547
junior <juniormathiias@gmail.com>
2023-12-27 19:25:29 -0300

    adicionado tasks e seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cadastrando as moedas..."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url: "https://png.pngtree.com/png-vector/20220527/ourmid/pngtree-bitcoin-logo-design-template-png-image_4749408.png"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url: "https://img1.gratispng.com/20180516/ouw/kisspng-ethereum-cryptocurrency-blockchain-logo-eos-io-crypto-5afc9ab9b20d86.8643914515265041217293.jpg"
+)
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DSH",
+    url: "https://www.pngall.com/wp-content/uploads/10/Dash-Crypto-Logo-PNG-Cutout.png"
+)
+
+puts "Moedas cadastradas com sucesso..."
