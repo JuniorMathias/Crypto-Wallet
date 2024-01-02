7ad14e30027ee4cd0b832c6386ee3df92277e3c5
junior <juniormathiias@gmail.com>
2024-01-02 15:59:29 -0300

    adicionado a tabela de referencia de miningType para Coins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_02_141202) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_02_184023) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_141202) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "mining_type_id"
+    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
   end
 
   create_table "mining_types", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_141202) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "coins", "mining_types"
 end
